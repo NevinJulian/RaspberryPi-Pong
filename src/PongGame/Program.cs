62cb1b8687383219d9fba5f33777adc00c925150
aceto <silvan.vifian@stud.hslu.ch>
2024-04-08 18:58:56 +0200

    Make it work after merge ;)

--- src/PongGame/Program.cs
+++ src/PongGame/Program.cs
@@ -15,7 +15,8 @@ internal class Program
     private static DateTime lastPaddleMove;
     private static Startscreen startscreen;
     private static bool gameRunning = false;
-    
+    private static bool showScore;
+
     static async Task Main(string[] args)
     {
         Console.WriteLine("Pong Game Start...");
@@ -29,16 +30,16 @@ internal class Program
         {
             if (gameRunning == false)
             {
-                DrawScore();
-                await Task.Delay(TimeSpan.FromSeconds(3)); // Wait for 3 seconds before going to startscreen
-                scoreboard.ResetPlayerScore();
-                DrawStartScreen();
-                exp.Joystick.JoystickChanged += (_, e) =>
+                if (showScore)
                 {
-                    gameRunning = true;
-                };
+                    DrawScore();
+                    await Task.Delay(TimeSpan.FromSeconds(2)); // Wait for 3 seconds before going to startscreen
+                    showScore = false;
+                }
+                DrawStartScreen();
+                exp.Joystick.JoystickChanged += (_, e) => { gameRunning = true; };
             }
-            else
+            else if (gameRunning == true)
             {
                 // Execute Game loop
                 HandleInput();
@@ -50,138 +51,134 @@ internal class Program
         }
 
 
-    static async Task SetupGame()
-    {
-        paddle = new Paddle(20, 0, 20, 7, DisplayHeight, 5);
-        ball = new Ball(30, 100, 1, 1, 10, DisplayHeight, DisplayWidth);
-        scoreboard = new Scoreboard();
-        startscreen = new Startscreen("Pong Game, Press any key to start");
-
-
-        await scoreboard.LoadPersistedHighScore();
-        scoreboard = new Scoreboard(0);
-        startscreen = new Startscreen("Pong Game, Press any key to start");
-    }
-
-    static void HandleInput()
-    {
-        exp.Joystick.JoystickChanged += (_, e) =>
+        static async Task SetupGame()
         {
-            if (e.Keys.HasFlag(Keys.Down) && IsAllowedToMovePaddle())
+            paddle = new Paddle(20, 0, 20, 7, DisplayHeight, 5);
+            ball = new Ball(30, 100, 1, 1, 10, DisplayHeight, DisplayWidth);
+            scoreboard = new Scoreboard();
+            await scoreboard.LoadPersistedHighScore();
+            startscreen = new Startscreen("Pong Game, Press any key to start");
+        }
+
+        static void HandleInput()
+        {
+            exp.Joystick.JoystickChanged += (_, e) =>
             {
-                paddle.MoveRight(GetAmountOfSteps(e.Keys));
-                lastPaddleMove = DateTime.UtcNow;
-            }
-            else if (e.Keys.HasFlag(Keys.Up) && IsAllowedToMovePaddle())
+                if (e.Keys.HasFlag(Keys.Down) && IsAllowedToMovePaddle())
+                {
+                    paddle.MoveRight(GetAmountOfSteps(e.Keys));
+                    lastPaddleMove = DateTime.UtcNow;
+                }
+                else if (e.Keys.HasFlag(Keys.Up) && IsAllowedToMovePaddle())
+                {
+                    paddle.MoveLeft(GetAmountOfSteps(e.Keys));
+                    lastPaddleMove = DateTime.UtcNow;
+                }
+            };
+
+            static bool IsAllowedToMovePaddle()
             {
-                paddle.MoveLeft(GetAmountOfSteps(e.Keys));
-                lastPaddleMove = DateTime.UtcNow;
-            }
-        };
-
-        static bool IsAllowedToMovePaddle()
-        {
-            // Limit paddle movement to every 100ms
-            return DateTime.UtcNow - lastPaddleMove > TimeSpan.FromMilliseconds(100);
-        }
-
-        static int GetAmountOfSteps(Keys keys)
-        {
-            return keys.HasFlag(Keys.Center)
-                ? 3
-                : 1;
-        }
-    }
-
-    static async Task UpdateGameState()
-    {
-        // Move the ball
-        ball.Move();
-
-        // Collision with walls
-        if (ball.IsCollidingWithLeftWall() || ball.IsCollidingWithRightWall())
-        {
-            // Reverse the x velocity of the ball to bounce off the wall
-            ball.Velocity = new PointF(-ball.Velocity.X, ball.Velocity.Y);
-        }
-        else if (ball.IsCollidingWithTopWall())
-        {
-            // Reverse the y velocity of the ball to bounce off the wall
-            ball.Velocity = new PointF(ball.Velocity.X, -ball.Velocity.Y);
-            exp.Buzzer.Beep(200);
-
-        }
-        else if (ball.IsCollidingWithPaddle(paddle))
-        {
-            // Reverse the y velocity of the ball to bounce off the paddle
-            ball.Velocity = new PointF(ball.Velocity.X, -ball.Velocity.Y);
-            exp.Buzzer.Beep(200);
-            exp.Buzzer.Beep(200);
-
-
-            // Increase player score
-            scoreboard.IncrementPlayerScore();
-            Console.WriteLine($"Player Score: {scoreboard.PlayerScore}");
-
-            // Increase ball speed
-            ball.Velocity = new PointF(ball.Velocity.X * 1.1f, ball.Velocity.Y * 1.1f);
-        }
-        else if (ball.IsCollidingWithBottomWall())
-        {
-            Console.WriteLine($"Game Over with score {scoreboard.PlayerScore}");
-
-            // Reset the ball position and velocity
-            ball.Reset();
-            exp.Buzzer.Beep(300);
-            exp.Buzzer.Beep(300);
-            exp.Buzzer.Beep(300);
-            // Check and update high score
-            if (scoreboard.UpdateHighScore())
-            {
-                // It is a high score
-                Console.WriteLine($"New High Score: {scoreboard.HighScore}");
-
-                // Persist high score
-                await scoreboard.PersistHighScore();
+                // Limit paddle movement to every 100ms
+                return DateTime.UtcNow - lastPaddleMove > TimeSpan.FromMilliseconds(100);
             }
 
-            GameStillRunning = false;
-            // Reset player score
-            scoreboard.ResetPlayerScore();
-            
-            // Go back to start screen
-            gameRunning = false;
+            static int GetAmountOfSteps(Keys keys)
+            {
+                return keys.HasFlag(Keys.Center)
+                    ? 3
+                    : 1;
+            }
+        }
+
+        static async Task UpdateGameState()
+        {
+            // Move the ball
+            ball.Move();
+
+            // Collision with walls
+            if (ball.IsCollidingWithLeftWall() || ball.IsCollidingWithRightWall())
+            {
+                // Reverse the x velocity of the ball to bounce off the wall
+                ball.Velocity = new PointF(-ball.Velocity.X, ball.Velocity.Y);
+            }
+            else if (ball.IsCollidingWithTopWall())
+            {
+                // Reverse the y velocity of the ball to bounce off the wall
+                ball.Velocity = new PointF(ball.Velocity.X, -ball.Velocity.Y);
+                exp.Buzzer.Beep(200);
+
+            }
+            else if (ball.IsCollidingWithPaddle(paddle))
+            {
+                // Reverse the y velocity of the ball to bounce off the paddle
+                ball.Velocity = new PointF(ball.Velocity.X, -ball.Velocity.Y);
+                exp.Buzzer.Beep(200);
+                exp.Buzzer.Beep(200);
+
+
+                // Increase player score
+                scoreboard.IncrementPlayerScore();
+                Console.WriteLine($"Player Score: {scoreboard.PlayerScore}");
+
+                // Increase ball speed
+                ball.Velocity = new PointF(ball.Velocity.X * 1.1f, ball.Velocity.Y * 1.1f);
+            }
+            else if (ball.IsCollidingWithBottomWall())
+            {
+                Console.WriteLine($"Game Over with score {scoreboard.PlayerScore}");
+
+                // Reset the ball position and velocity
+                ball.Reset();
+                // Check and update high score
+                if (scoreboard.UpdateHighScore())
+                {
+                    // It is a high score
+                    Console.WriteLine($"New High Score: {scoreboard.HighScore}");
+
+                    // Persist high score
+                    await scoreboard.PersistHighScore();
+                }
+                exp.Buzzer.Beep(300);
+                exp.Buzzer.Beep(300);
+                exp.Buzzer.Beep(300);
+                // Reset player score
+                scoreboard.ResetPlayerScore();
+
+                // Go back to start screen
+                gameRunning = false;
+                showScore = true;
+            }
+        }
+
+        static void DrawStartScreen()
+        {
+            Graphics g = exp.Display.Graphics;
+            g.Clear(Color.Black);
+
+            startscreen.Draw(g, scoreboard.HighScore.ToString());
+
+            exp.Display.Update(); // Update display
+        }
+
+        static void DrawGame()
+        {
+            Graphics g = exp.Display.Graphics;
+            g.Clear(Color.Black);
+
+            paddle.Draw(g);
+            ball.Draw(g);
+
+            exp.Display.Update(); // Update display
+        }
+
+        static void DrawScore()
+        {
+            Graphics g = exp.Display.Graphics;
+            g.Clear(Color.Black);
+
+            scoreboard.Draw(g);
+
+            exp.Display.Update();
         }
     }
-    
-    static void DrawStartScreen()
-    {
-        Graphics g = exp.Display.Graphics;
-        g.Clear(Color.Black);
-
-        startscreen.Draw(g, scoreboard.HighScore.ToString());
-
-        exp.Display.Update(); // Update display
-    }
-
-    static void DrawGame()
-    {
-        Graphics g = exp.Display.Graphics;
-        g.Clear(Color.Black);
-
-        paddle.Draw(g);
-        ball.Draw(g);
-
-        exp.Display.Update(); // Update display
-    }
-
-    static void DrawScore()
-    {
-        Graphics g = exp.Display.Graphics;
-        g.Clear(Color.Black);
-
-        scoreboard.Draw(g);
-
-        exp.Display.Update();
-    }
-}
+};
